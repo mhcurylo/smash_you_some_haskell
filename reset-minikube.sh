666706880c302834eddc731382f1e7e919c5b6c0
Mateusz Curylo <mhcurylo@gmail.com>
2020-11-02 02:09:07 +0000

    Deployments

--- reset-minikube.sh
+++ reset-minikube.sh
@@ -5,4 +5,7 @@ echo "Reseting minikube for Smash you Some Haskell!"
 minikube delete
 
 minikube start --cpus 6
+minikube kubectl -- apply -f deployments/postgres.yaml
+minikube kubectl -- apply -f deployments/node-1-nolr.yaml
+minikube kubectl -- apply -f deployments/haskell-N.yaml
 
